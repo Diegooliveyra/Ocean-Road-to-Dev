8023b5b6385779e4fd5f026aeefd81d5abbc1c6a
diegooliveyra <diegooliveirabryto@gmail.com>
2020-11-18 22:46:53 -0300

    feito o menu mobile

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,21 @@
-const menuBtn = document.querySelector('.menu-btn');
-menuBtn.addEventListener('click', function(){
-  menuBtn.classList.toggle('menu-btn-open')
-})
+//Menu mobile
+const $body = document.querySelector("body");
+const $menu = document.querySelector('.menu')
+const links = document.querySelectorAll(".menu a");
+const $menuBtn = document.querySelector('.menu-btn');
+
+
+for (i = 0; i < links.length; i++) {
+  links[i].onclick = activeMenu;
+}
+
+$menuBtn.addEventListener("click", activeMenu);
+
+function activeMenu() {
+  if (window.innerWidth < 768) {
+    $menu.classList.toggle('menu-open')
+    $menuBtn.classList.toggle('menu-btn-open')
+    $body.classList.toggle("noscrol");
+  }
+}
+
